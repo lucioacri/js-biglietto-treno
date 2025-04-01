6b6381cbc7d3e46ea8ad1741cecece7b2f51436b
lucioacri <lucioacri@gmail.com>
2025-04-01 18:14:47 +0200

    add samuel's fixes

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
 // INFORMAZIONI
 
-const distance = parseInt(prompt("Quanti Km vuoi percorrere?", "50"));
-console.log("distance", distance);
-
-const ages = parseInt(prompt("Quanti anni hai?", "18"));
-console.log("ages", ages);
-
 const kmPrice = 0.21;
-
-const price = distance * kmPrice;
-console.log("price", price);
-
-const discount20 = (price * 20) / 100;
-const discount40 = (price * 40) / 100;
-
 let outputText;
 
-// ALGORITMO
-if (!isNaN(distance) && !isNaN(ages)) {
-  if (ages < 18) {
-    outputText = price - discount20;
-  } else if (ages > 65) {
-    outputText = price - discount40;
-  } else {
-    outputText = price;
-  }
+const distance = parseInt(prompt("Quanti Km vuoi percorrere?", "50"));
+console.log("distance", distance);
+if (isNaN(distance)) {
+  alert("Distanza non corretta");
 } else {
-  alert("Inserisci un numero");
+  const ages = parseInt(prompt("Quanti anni hai?", "18"));
+  console.log("ages", ages);
+  if (isNaN(ages)) {
+    alert("Età non corretta");
+  } else {
+    const price = distance * kmPrice;
+    console.log("price", price);
+    if (ages < 18) {
+      const discount20 = (price * 20) / 100;
+      outputText = price - discount20;
+    } else if (ages > 65) {
+      const discount40 = (price * 40) / 100;
+      outputText = price - discount40;
+    } else {
+      outputText = price;
+    }
+    alert("Il prezzo del biglietto è €" + outputText.toFixed(2));
+  }
 }
-
-alert("Il prezzo del biglietto è €" + outputText.toFixed(2));
