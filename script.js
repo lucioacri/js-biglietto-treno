25e9a0dca3e25c4f5321c8286b5a37accfb8cd58
lucioacri <lucioacri@gmail.com>
2025-04-01 14:39:39 +0200

    add everything in one push, sorry..

---
+++ script.js
@@ -0,0 +1,37 @@
+// INFORMAZIONI
+
+const distance = parseInt(prompt("Quanti Km vuoi percorrere?", 50));
+console.log("distance", distance);
+
+const ages = parseInt(prompt("Quanti anni hai?", 18));
+console.log("ages", ages);
+
+let kmPrice = 0.21;
+
+const price = distance * kmPrice;
+console.log("price", price);
+
+const discount20 = (price * 20) / 100;
+const discount40 = (price * 40) / 100;
+
+const final20 = price - discount20;
+console.log("final20", final20);
+
+const final40 = price - discount40;
+console.log("final40", final40);
+
+let outputText = price;
+
+// ALGORITMO
+
+if (ages < 18) {
+  outputText = final20;
+}
+if (ages >= 18) {
+  outputText = price;
+}
+if (ages > 65) {
+  outputText = final40;
+}
+
+alert("Il prezzo del biglietto è € " + outputText.toFixed(2));
